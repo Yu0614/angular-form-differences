727d9725fd0f6b25ea3a63adc14f4642682fe5d0
Yu0614 <azu369yu@gmail.com>
2020-09-22 15:39:22 +0900

    feat: update tempalte driven componet & introduce template driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 import { ReactiveComponent } from './reactive/reactive.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [AppComponent, TemplateDrivenComponent, ReactiveComponent],
-    imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+    imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-template-driven',
@@ -7,6 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateDrivenComponent implements OnInit {
     constructor() {}
+    public model = {
+        mail: '',
+        password: '',
+    };
+
+    @ViewChild('form')
+    form: NgForm;
 
     ngOnInit(): void {}
+
+    /**
+     * Submit form values
+     */
+    submit() {
+        console.log('submit');
+        window.alert(JSON.stringify(this.form.value));
+    }
+
+    /**
+     * Fill emailAddress with specific email
+     *  L shows example for filling email type
+     *  L Overwrites existing value
+     */
+    fillMail() {
+        this.model.mail = 'sample@example.com';
+        window.alert(JSON.stringify(this.model));
+    }
+
+    /**
+     * Fill password with specific password
+     *  L Shows example for filling password type
+     *  L Overwrites existing value
+     */
+    fillPassword() {
+        this.model.password = 'pass1word';
+        window.alert(JSON.stringify(this.model));
+    }
+
+    /**
+     * Delete form values
+     */
+    deleteValue() {
+        this.model.mail = '';
+        this.model.password = '';
+    }
 }
